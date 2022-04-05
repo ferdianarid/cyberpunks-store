fa53f2c47242b0eb1e676b7362ac285c0791631c
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-06 00:36:25 +0700

    feature/add apis dummy

--- apis/books.js
+++ apis/books.js
@@ -1,4 +1,22 @@
 export const books = [{
+        id: 6,
+        title: "Eloquent JavaScript",
+        image: "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
+        description: "The fundamental rules are simple and clear, but programs built on top of these rules"
+    },
+    {
+        id: 7,
+        title: "Pragmatic Programmer",
+        image: "https://images-na.ssl-images-amazon.com/images/I/71Nxk9VhSTL.jpg",
+        description: "Is the Pragmatic Programmer a good read for a newbie to computer programming"
+    },
+    {
+        id: 3,
+        title: "Design Pattern Oreilly",
+        image: "https://images-na.ssl-images-amazon.com/images/I/917iQZBR91L.jpg",
+        description: "Learn how to write beautiful, structured, and maintainable JavaScript"
+    },
+    {
         id: 1,
         title: "The Design of Every Day Things",
         image: "https://images.tokopedia.net/img/cache/500-square/product-1/2017/10/16/14481426/14481426_471416c0-7776-4a8f-8ee8-02a0e5027c3c_334_500.jpg",
@@ -11,13 +29,13 @@ export const books = [{
         description: "If a dog chews shoes whose shoes does he choose?"
     },
     {
-        id: 3,
+        id: 4,
         title: "This is Marketing",
         image: "https://i.pinimg.com/736x/4d/20/bb/4d20bb46c7a2ae978ac809b63507c4bc.jpg",
         description: "If a dog chews shoes whose shoes does he choose?"
     },
     {
-        id: 4,
+        id: 5,
         title: "Value Proposition Design",
         image: "https://www.sprinthink.id/wp-content/uploads/2019/11/img-value-proposition-1.jpg",
         description: "If a dog chews shoes whose shoes does he choose?"
---
+++ apis/products.js
@@ -0,0 +1,25 @@
+export const computer = [{
+        id: 1,
+        title: "Computer"
+    },
+    {
+        id: 2,
+        title: "Power Supply"
+    },
+    {
+        id: 3,
+        title: "Harddisk External"
+    },
+    {
+        id: 4,
+        title: "Mouse Optic"
+    },
+    {
+        id: 5,
+        title: "Charger"
+    },
+    {
+        id: 6,
+        title: "Power Bank"
+    }
+]
